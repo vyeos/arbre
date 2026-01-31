8889203fdaa9546409226f9a2d347577097682d1
vyeos <rudranpatel0980@gmail.com>
2026-01-31 10:20:14 +0530

    refactor: file structure

--- lib/elysia-client.ts
+++ lib/elysia-client.ts
@@ -1,4 +1,4 @@
-import { App } from "@/app/api/elysia/[...all]/route";
+import type { App } from "@/app/api/elysia/[...all]/route";
 import { edenTreaty } from "@elysiajs/eden";
 
 const baseUrl = process.env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000";
